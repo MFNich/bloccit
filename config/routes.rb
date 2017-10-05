13adeabc7454761c528c60c4ba72c4100bc2c934
Matt Nichols <nich331@gmail.com>
2017-10-04 20:45:18 -0400

    checkpoint 24 rails complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
-   resources :topics do
-     resources :posts, except: [:index]
-   end
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
-   get 'about' => 'welcome#about'
+  resources :users, only: [:new, :create]
+  
+  get 'about' => 'welcome#about'
 
   root 'welcome#index'
 end
