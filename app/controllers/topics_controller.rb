3d2176862bca8801145667e38b4d1554e3b2fcd1
Matt Nichols <nich331@gmail.com>
2017-10-20 16:19:16 -0400

    checkpoint 28 rails complete

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,8 @@
 class TopicsController < ApplicationController
+
+  before_action :require_sign_in, except: [:index, :show]
+  before_action :authorize_user, except: [:index, :show]
+
   def index
     @topics = Topic.all
   end
@@ -27,34 +31,41 @@ class TopicsController < ApplicationController
   end
 
   def update
-     @topic = Topic.find(params[:id])
+    @topic = Topic.find(params[:id])
 
-     @topic.assign_attributes(topic_params)
+    @topic.assign_attributes(topic_params)
 
-     if @topic.save
-        flash[:notice] = "Topic was updated."
-       redirect_to @topic
-     else
-       flash.now[:alert] = "Error saving topic. Please try again."
-       render :edit
-     end
-   end
+    if @topic.save
+      flash[:notice] = "Topic was updated."
+      redirect_to @topic
+    else
+      flash.now[:alert] = "Error saving topic. Please try again."
+      render :edit
+    end
+  end
 
-   def destroy
-     @topic = Topic.find(params[:id])
+  def destroy
+    @topic = Topic.find(params[:id])
 
-     if @topic.destroy
-       flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
-       redirect_to action: :index
-     else
-       flash.now[:alert] = "There was an error deleting the topic."
-       render :show
-     end
-   end
+    if @topic.destroy
+      flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
+      redirect_to action: :index
+    else
+      flash.now[:alert] = "There was an error deleting the topic."
+      render :show
+    end
+  end
 
-   private
+  private
 
-   def topic_params
-     params.require(:topic).permit(:name, :description, :public)
-   end
+  def topic_params
+    params.require(:topic).permit(:name, :description, :public)
+  end
+
+  def authorize_user
+    unless current_user.admin?
+      flash[:alert] = "You must be an admin to do that."
+      redirect_to topics_path
+    end
+  end
 end
